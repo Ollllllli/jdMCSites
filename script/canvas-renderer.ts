62202e305e00336f4b0a831068a140434d351de3
Oliver Lenehan <55334808+sunsetkookaburra@users.noreply.github.com>
2021-01-19 16:53:48 +1100

    some progress toward canvas-renderer, stale now

--- script/canvas-renderer.ts
+++ script/canvas-renderer.ts
@@ -1,3 +1,73 @@
+/****************** GLOBALS ******************/
+
+const _global_render_var = {
+
+  /** The key is a string in format `renderWidth:renderHeight|textureWidth:textureHeight|shapeKey`.
+   * maps a given renderSize and shape, and each index stores the texture index.
+   * Appears as differing Uint arrays to save space. */
+  bakedMaps: new Map<string, Uint8Array | Uint16Array | Uint32Array>(),
+
+  /** Key is a texture's `width:height`.
+   * the float32 is used as u,v,... a pair for each index in the texture */
+  bakedTexUVs: new Map<string, Float32Array>(),
+
+  /** Key = `shapeKey` */
+  uvProjectors: new Map<string, UVProjector>(),
+};
+
+const _global_render_func = {
+
+  uvFromPoint(point: PointVector, width: number, height: number): UVVector {
+    return [point[0] / width, point[1] / height] as any;
+  },
+
+  pointFromUV(coord: UVVector, width: number, height: number): PointVector {
+    return [Math.abs(Math.floor(coord[0] * width)), Math.abs(Math.floor(coord[1] * height))] as any;
+  },
+
+  /** Returns a `Uint32Array` mapping each pixel in the render to a pixel in the texture. */
+  bake({shapeKey, shape, textureSize, renderSize}: {
+    shapeKey: string,
+    shape: Quad<UVVector>,
+    textureSize: {width: number, height: number},
+    renderSize: {width: number, height: number},
+  }): Uint32Array {
+    const bakeKey =
+      `${renderSize.width}:${renderSize.height}|` +
+      `${textureSize.width}:${textureSize.height}|` +
+      `${shapeKey}`;
+    // see if textureMap has already been cached
+    if (!_global_render_var.bakedMaps.has(bakeKey)) {
+      // see if a uvprojector exists for the shape already
+      if (!_global_render_var.uvProjectors.has(shapeKey)) {
+        _global_render_var.uvProjectors.set(shapeKey, new UVProjector(shape));
+      }
+      const projector = _global_render_var.uvProjectors.get(shapeKey)!;
+    }
+    if (!this.bakedMaps.has(textureDim)) {
+      const renderMap = new Uint32Array(this.renderWidth * this.renderHeight);
+      const renderTextureUVBake: UVVector[] = [];
+      for (let y = 0; y < this.renderHeight; y++) {
+        for (let x = 0; x < this.renderWidth; x++) {
+          const renderIndex = y * this.renderHeight + x;
+          const renderUV = this.uv([x, y] as any, this.renderWidth, this.renderHeight);
+          const textureUV = projector.project(renderUV);
+          renderTextureUVBake.push(textureUV);
+          const textureXY = this.point(textureUV, texture.width, texture.height);
+          const textureIndex = textureXY[1] * texture.height + textureXY[0];
+          renderMap[renderIndex] = textureIndex;
+        }
+      }
+      this.bakedMaps.set(textureDim, renderMap);
+      this.renderToTextureBakedUV.set(textureDim, renderTextureUVBake);
+    }
+    const bakedMap = this.bakedMaps.get(textureDim)!;
+    return bakedMap;
+  },
+};
+
+/****************** END GLOBALS ******************/
+
 interface RGBA {
   r: number;
   g: number;
@@ -5,36 +75,54 @@ interface RGBA {
   a: number;
 }
 
-class Vector2D {
-  protected type = "vec2";
-  constructor(public x: number, public y: number) {}
-  get size() { return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)); }
-  // get array() { return [this.x, this.y] as const }
-  add(vector: Vector2D) {
-    return new Vector2D(this.x + vector.x, this.y + vector.y);
-  }
-  sub(vector: Vector2D) {
-    return new Vector2D(this.x - vector.x, this.y - vector.y);
-  }
-  dotProduct(vector: Vector2D) { return this.x * vector.x + this.y * vector.y; }
-  unit() {
-    return new Vector2D(this.x / this.size, this.y / this.size);
-  }
-  normal(side: "left" | "right") {
-    if (side == "left")
-      return new Vector2D(this.y, -this.x);
-    else
-      return new Vector2D(-this.y, this.x);
-  }
-}
-
 type Brand<T, BrandType extends string> = T & { [Brand in BrandType]: true }
-type ArrayND<T extends number> = { readonly [N in T]: number; } & { readonly length: number; }
+type ArrayND<T extends number> = { [N in T]: number; } & { readonly length: number; }
 type Array2D = ArrayND<0|1>;
 type Array3D = ArrayND<0|1|2>;
 type Array4D = ArrayND<0|1|2|3>;
 type UVVector = Brand<Array2D, "UVVector">;
 type PointVector = Brand<Array2D, "PointVector">;
+
+interface Vector2D extends Array2D {};
+class Vector2D extends Float32Array {
+  //array: Array2D & Float32Array = new Float32Array(2) as any;
+  constructor(from: Array2D) { super(from); }
+  size() {
+    return Math.sqrt(Math.pow(this[0], 2) + Math.pow(this[1], 2));
+  }
+  // get array() { return [this.x, this.y] as const }
+  add(vector: Array2D) {
+    this[0] = this[0] + vector[0];
+    this[1] = this[1] + vector[1];
+    return this;
+  }
+  sub(vector: Array2D) {
+    this[0] = this[0] - vector[0];
+    this[1] = this[1] - vector[1];
+    return this;
+  }
+  dotProduct(withVector: Array2D) {
+    return this[0] * withVector[0] + this[1] * withVector[1];
+  }
+  unit() {
+    const magnitude = this.size();
+    this[0] = this[0] / magnitude;
+    this[1] = this[1] / magnitude;
+    return this;
+  }
+  normal(side: "left" | "right") {
+    const temp = this[0];
+    if (side == "left") {
+      this[0] = this[1]
+      this[1] = -temp;
+    }
+    else {
+      this[0] = -this[1]
+      this[1] = temp;
+    }
+    return this;
+  }
+}
 
 interface Quad<T extends Array2D = Array2D> {
   topLeft: T;
@@ -86,86 +174,50 @@ async function loadTexture(src: string | null) {
 }
 
 class UVProjector {
-  private topLeft: Vector2D;
-  private topRight: Vector2D;
-  private bottomLeft: Vector2D;
-  private bottomRight: Vector2D;
-  private n0: Vector2D;
-  private n1: Vector2D;
-  private n2: Vector2D;
-  private n3: Vector2D;
-  constructor(shape: Quad<UVVector>) {
-    this.topLeft = new Vector2D(shape.topLeft[0], shape.topLeft[1]);
-    this.topRight = new Vector2D(shape.topRight[0], shape.topRight[1]);
-    this.bottomLeft = new Vector2D(shape.bottomLeft[0], shape.bottomLeft[1]);
-    this.bottomRight = new Vector2D(shape.bottomRight[0], shape.bottomRight[1]);
-    this.n0 = this.topLeft.sub(this.bottomLeft).normal("right").unit();
-    this.n1 = this.bottomRight.sub(this.bottomLeft).normal("left").unit();
-    this.n2 = this.topRight.sub(this.bottomRight).normal("left").unit();
-    this.n3 = this.topRight.sub(this.topLeft).normal("right").unit();
-  }
+
+  private n0
+    = new Vector2D(this.shape.topLeft)
+    .sub(this.shape.bottomLeft)
+    .normal("right")
+    .unit();
+    
+  private n1
+    = new Vector2D(this.shape.bottomRight)
+    .sub(this.shape.bottomLeft)
+    .normal("left")
+    .unit();
+
+  private n2
+    = new Vector2D(this.shape.topRight)
+    .sub(this.shape.bottomRight)
+    .normal("left")
+    .unit();
+
+  private n3
+    = new Vector2D(this.shape.topRight)
+    .sub(this.shape.topLeft)
+    .normal("right")
+    .unit();
+
+  constructor(private shape: Quad<UVVector>){}
+
   project(coord: UVVector): UVVector {
-    const coordVector2D = new Vector2D(coord[0], coord[1]);
-    const du0 = Math.abs(coordVector2D.sub(this.bottomLeft).dotProduct(this.n0));
-    const du1 = Math.abs(coordVector2D.sub(this.topRight).dotProduct(this.n2));
-    const dv0 = Math.abs(coordVector2D.sub(this.bottomRight).dotProduct(this.n1));
-    const dv1 = Math.abs(coordVector2D.sub(this.topLeft).dotProduct(this.n3));
+    // create a coordVec to hold temp values
+    const coordVec = new Vector2D(coord);
+    // du0 = perpendicular distance to P from left side.
+    const du0 = Math.abs(coordVec.sub(this.shape.bottomLeft).dotProduct(this.n0));
+    coordVec.set(coord);
+    // du1 = perpendicular distance to P from right side.
+    const du1 = Math.abs(coordVec.sub(this.shape.topRight).dotProduct(this.n2));
+    coordVec.set(coord);
+    // dv0 = perpendicular distance to P from bottom side.
+    const dv0 = Math.abs(coordVec.sub(this.shape.bottomRight).dotProduct(this.n1));
+    coordVec.set(coord);
+    // dv1 = perpendicular distance to P from top side.
+    const dv1 = Math.abs(coordVec.sub(this.shape.topLeft).dotProduct(this.n3));
     const u = du0 / (du0 + du1);
-    const v = 1 - (dv0 / (dv0 + dv1)); // 1 - to convert to computer-cartesian
+    const v = dv1 / (dv1 + dv0);
     return [u, v] as any;
-  }
-}
-
-// NEED TO PREBAKE
-class RenderMapper {
-  /** The key is a string in format `width:height`. */
-  readonly bakedMaps: Map<string, Uint32Array>
-    = new Map();
-  readonly renderToTextureBakedUV: Map<string, UVVector[]>
-    = new Map();
-
-  constructor(readonly renderWidth: number, readonly renderHeight: number) {}
-
-  private uv(point: PointVector, width: number, height: number): UVVector {
-    return [point[0] / width, point[1] / height] as any;
-  }
-
-  private point(coord: UVVector, width: number, height: number): PointVector {
-    return [Math.floor(coord[0] * width), Math.floor(coord[1] * height)] as any;
-  }
-
-  pointInRender(coord: UVVector) {
-    return this.point(coord, this.renderWidth, this.renderHeight);
-  }
-
-  uvInRender(point: PointVector) {
-    return this.uv(point, this.renderWidth, this.renderHeight);
-  }
-
-  // eventually check that point within shape
-  /** Returns a `Uint32Array` mapping each pixel in the render to a pixel in the texture. */
-  bake(shape: Quad<UVVector>, texture: ImageData): Uint32Array {
-    const textureDim = `${JSON.stringify(shape)}${texture.width}:${texture.height}`;
-    const projector = new UVProjector(shape);
-    if (!this.bakedMaps.has(textureDim)) {
-      const renderMap = new Uint32Array(this.renderWidth * this.renderHeight);
-      const renderTextureUVBake: UVVector[] = [];
-      for (let y = 0; y < this.renderHeight; y++) {
-        for (let x = 0; x < this.renderWidth; x++) {
-          const renderIndex = y * this.renderHeight + x;
-          const renderUV = this.uv([x, y] as any, this.renderWidth, this.renderHeight);
-          const textureUV = projector.project(renderUV);
-          renderTextureUVBake.push(textureUV);
-          const textureXY = this.point(textureUV, texture.width, texture.height);
-          const textureIndex = textureXY[1] * texture.height + textureXY[0];
-          renderMap[renderIndex] = textureIndex;
-        }
-      }
-      this.bakedMaps.set(textureDim, renderMap);
-      this.renderToTextureBakedUV.set(textureDim, renderTextureUVBake);
-    }
-    const bakedMap = this.bakedMaps.get(textureDim)!;
-    return bakedMap;
   }
 }
 
@@ -185,10 +237,8 @@ class Renderer {
   private renderContext: CanvasRenderingContext2D
     = this.renderCanvas.getContext("2d")!;
   /** The key is a string in format `width:height`. */
-  private renderMap: RenderMapper;
 
   constructor(readonly width: number, readonly height: number) {
-    this.renderMap = new RenderMapper(width, height);
     this.renderCanvas.width = width;
     this.renderCanvas.height = height;
     this.renderContext.fillStyle = "#fff";
@@ -198,10 +248,10 @@ class Renderer {
     this.renderContext.clearRect(0, 0, this.renderCanvas.width, this.renderCanvas.height);
     // prime for drawing, by filling a quad on the canvas
     this.renderContext.beginPath();
-    const topLeft = this.renderMap.pointInRender(shape.topLeft);
-    const topRight = this.renderMap.pointInRender(shape.topRight);
-    const bottomLeft = this.renderMap.pointInRender(shape.bottomLeft);
-    const bottomRight = this.renderMap.pointInRender(shape.bottomRight);
+    const topLeft = RenderMapper.pointFromUV(shape.topLeft, this.width, this.height);
+    const topRight = RenderMapper.pointFromUV(shape.topRight, this.width, this.height);
+    const bottomLeft = RenderMapper.pointFromUV(shape.bottomLeft, this.width, this.height);
+    const bottomRight = RenderMapper.pointFromUV(shape.bottomRight, this.width, this.height);
     this.renderContext.moveTo(topLeft[0], topLeft[1]);
     this.renderContext.lineTo(bottomLeft[0], bottomLeft[1]+1);
     this.renderContext.lineTo(bottomRight[0]+1, bottomRight[1]+1);
@@ -261,3 +311,14 @@ class Renderer {
     canvasContext.drawImage(this.renderCanvas, 0, 0);
   }
 }
+
+
+
+/*
+RenderMapper creation
+
+things to store:
+render<size> => texture<size>
+bake(shape, renderSize, textureSize)
+
+*/
